8e2d9b353bd367e2a4ccac3d7f6f0f230d8a003b
Philip Mathieu <philip.eng.mathieu@gmail.com>
2014-02-26 22:39:34 -0500

    More sophisticated test

--- TestClient.py
+++ TestClient.py
@@ -5,5 +5,16 @@ port = 60000
 size = 1024 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 s.connect((host,port)) 
-s.send('toggle,blue,2') 
+s.send('toggle,blue,2')
+sleep(1)
+s.send('toggle,red,2')
+sleep(1)
+s.send('toggle,green,2')
+sleep(1)
+s.send('toggle,red,2')
+sleep(1)
+s.send('toggle,blue,2')
+sleep(1)
+s.send('toggle,green,2')
+sleep(1)
 print 'Sent'
